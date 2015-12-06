97ce4dfb01f60a3c3e8128357e096412c4654ccf
tale <giangnguyen.tale@gmail.com>
2015-12-06 15:56:01 +0700

    ! make sample to be awesome

--- app/src/main/java/com/tale/viewpresentationsample/MainActivity.java
+++ app/src/main/java/com/tale/viewpresentationsample/MainActivity.java
@@ -8,13 +8,16 @@ import android.view.ViewGroup;
 import com.tale.viewpresentation.ScreenPresenter;
 import com.tale.viewpresentation.animator.SlideInBottomAnimator;
 import com.tale.viewpresentation.animator.SlideInLeftAnimator;
+import com.tale.viewpresentation.animator.SlideInRightAnimator;
 import com.tale.viewpresentation.animator.SlideInTopAnimator;
 import com.tale.viewpresentation.animator.SlideOutBottomAnimator;
+import com.tale.viewpresentation.animator.SlideOutLeftAnimator;
 import com.tale.viewpresentation.animator.SlideOutRightAnimator;
 import com.tale.viewpresentation.animator.SlideOutTopAnimator;
 
 public class MainActivity extends AppCompatActivity {
 
+  private static final int ANIMATION_DURATION = 400;
   private ScreenPresenter screenPresenter;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
@@ -39,22 +42,32 @@ public class MainActivity extends AppCompatActivity {
     final ScreenView screenView = new ScreenView(this);
     screenView.setName("Top");
     screenView.setBackgroundColor(Color.BLUE);
-    screenPresenter.show(screenView, new SlideInTopAnimator(1000), new SlideOutBottomAnimator(1000),
-        new SlideInBottomAnimator(1000), new SlideOutTopAnimator(1000), true);
+    screenPresenter.show(screenView, null, new SlideOutTopAnimator(ANIMATION_DURATION),
+        new SlideInTopAnimator(ANIMATION_DURATION), null, true);
   }
 
   public void showLeft(View view) {
     final ScreenView screenView = new ScreenView(this);
     screenView.setName("Left");
     screenView.setBackgroundColor(Color.GREEN);
-    screenPresenter.show(screenView, new SlideInLeftAnimator(1000), new SlideOutRightAnimator(1000),
-        null, null, true);
+    screenPresenter.show(screenView, new SlideInLeftAnimator(ANIMATION_DURATION), new SlideOutRightAnimator(ANIMATION_DURATION),
+        new SlideInRightAnimator(ANIMATION_DURATION), new SlideOutLeftAnimator(ANIMATION_DURATION), true);
   }
 
   public void showRight(View view) {
+    final ScreenView screenView = new ScreenView(this);
+    screenView.setName("Right");
+    screenView.setBackgroundColor(Color.YELLOW);
+    screenPresenter.show(screenView, new SlideInRightAnimator(ANIMATION_DURATION), new SlideOutLeftAnimator(ANIMATION_DURATION),
+        new SlideInLeftAnimator(ANIMATION_DURATION), new SlideOutRightAnimator(ANIMATION_DURATION), true);
   }
 
   public void showBottom(View view) {
+    final ScreenView screenView = new ScreenView(this);
+    screenView.setName("Bottom");
+    screenView.setBackgroundColor(Color.MAGENTA);
+    screenPresenter.show(screenView, new SlideInBottomAnimator(ANIMATION_DURATION), null,
+        null, new SlideOutBottomAnimator(ANIMATION_DURATION), true);
   }
 
   @Override public void onBackPressed() {
